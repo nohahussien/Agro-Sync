755f652884a3e52dbfdeb5ccdf46ca9d71185508
Miguel <miguelmateo1985@gmail.com>
2026-01-21 13:55:07 +0100

    quito las credenciales de productos anteriores.

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings
 from typing import Optional
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:12345678@localhost/teatrAIDB"
-    SECRET_KEY: str = "12345678"
+    DATABASE_URL: str = ""
+    SECRET_KEY: str = ""
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
